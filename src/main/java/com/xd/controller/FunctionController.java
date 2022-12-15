7fb26a3020955a5044f99bdf6149b23c894abf5e
暖阳 <1784556173@qq.com>
2022-12-15 13:19:08 +0800

    初始化

--- src/main/java/com/xd/controller/FunctionController.java
+++ src/main/java/com/xd/controller/FunctionController.java
@@ -3,12 +3,14 @@ package com.xd.controller;
 
 import com.config.Result.Result;
 import com.xd.service.FunctionService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class FunctionController {
     @Resource
     private FunctionService functionService;
